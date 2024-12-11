b1b1419a5789ce010d48f78cf89a57276c9ada87
Vladislav Katok <katokvladislav@gmail.com>
2024-12-12 02:37:18 +0300

    add submission

---
+++ shared/src/commonMain/kotlin/by/funduk/model/Submission.kt
@@ -0,0 +1,15 @@
+package by.funduk.model
+
+import kotlinx.serialization.Serializable
+import kotlinx.datetime.LocalDateTime
+
+@Serializable
+data class Submission(
+    val id: Int? = null,
+    val taskId: Int,
+    val userId: Int,
+    val submitTime: LocalDateTime,
+    val code: String,
+    val language: Language,
+    val testInfo: TestInfo? = null,
+)
